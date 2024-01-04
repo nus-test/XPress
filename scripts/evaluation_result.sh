ee81ba38564b64f6a12fbb2ed665245c7a98b852
Twilight-Shuxin <1902605778@qq.com>
2024-01-04 16:24:46 +0000

    Update scripts

--- scripts/evaluation_result.sh
+++ scripts/evaluation_result.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 export RESULT_DIR=$1
 export ROUND=$2
-export TIME=$3
-cd XPress
-docker build -t xpress_eval .
-cd scripts
-./evaluation_get.sh $RESULT_DIR $ROUND $TIME &
-./evaluation_get2.sh $RESULT_DIR $ROUND $TIME &
-wait
+export COVER=$3
 
+configs=("p_r" "p_nr" "s_nr" "s_r")
+generators=("Com" "XQGen" "XPress")
+
+
+for i in $(eval echo {1..$2} );
+do
+    for generator in ${generators[@]};
+    do 
+        for config in ${configs[@]};
+        do 
+            python3 unique_check.py $RESULT_DIR $config $i $generator $COVER
+        done
+    done
+done   
+for i in $(eval echo {1..$2} );
+do 
+    python3 unique_count.py $RESULT_DIR $i
+done
+python3 cal_avg.py $RESULT_DIR $i
